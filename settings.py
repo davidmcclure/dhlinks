3eb52cddfdfef7286020d061818b010d46cb4e76
David McClure <david.mcclure@virginia.edu>
2011-05-27 17:46:38 -0400

    small changes to model

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'dhlinks',                      # Or path to database file if using sqlite3.
         'USER': 'david',                      # Not used with sqlite3.
         'PASSWORD': 'iloveyale09',                  # Not used with sqlite3.
