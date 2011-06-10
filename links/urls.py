85755cbbc9382bc4d20b89fa4a8c5732be44bbb5
David McClure <david.mcclure@virginia.edu>
2011-06-10 10:29:57 -0400

    all formatting changes made

--- links/urls.py
+++ links/urls.py
@@ -6,15 +6,15 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('links.views',
 
-	(r'^$', 'frontpage'),	
-	(r'^(?P<page_number>\d+)$', 'frontpage'),
-	(r'^new$', 'new'),
-	(r'^new/(?P<page_number>\d+)$', 'new'),	
-	(r'^submit$', 'submit'),
-	(r'^login$', 'login'),
-	(r'^logout', 'logout'),
-	(r'^comments/(?P<submission_id>\d+)$', 'comments'),
-	(r'^upvote/(?P<submission_id>\d+)$', 'upvote'),
-	(r'^register$', 'register')
+    (r'^$', 'frontpage'),
+    (r'^(?P<page_number>\d+)$', 'frontpage'),
+    (r'^new$', 'new'),
+    (r'^new/(?P<page_number>\d+)$', 'new'),
+    (r'^submit$', 'submit'),
+    (r'^login$', 'login'),
+    (r'^logout', 'logout'),
+    (r'^comments/(?P<submission_id>\d+)$', 'comments'),
+    (r'^upvote/(?P<submission_id>\d+)$', 'upvote'),
+    (r'^register$', 'register')
 
 )
