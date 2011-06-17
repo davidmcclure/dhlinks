cbdaa5dedc91928130ac5808c7b97e6bdca168c9
David McClure <david.mcclure@virginia.edu>
2011-06-17 17:40:26 -0400

    abstracted letterer stuff into separate class, shell for new LogoFX class

---
+++ links/static/js/LogoFX.js
@@ -0,0 +1,29 @@
+var LogoFX = new Class ({
+
+    Implements: [Options, Events],
+
+    options: {
+
+    },
+
+    initialize: function(options) {
+
+        this.setOptions(options);
+
+    },
+
+    materialize_logo: function() {
+
+
+
+    },
+
+});
+
+
+// dev usage
+
+// window.addEvent('domready', function() {
+//     var fx = new InterfaceFX;
+//     fx.logoAppear();
+// });
