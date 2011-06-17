a29c34f69b9b960f6a270128b0cc3bcd0f19a8b9
David McClure <david.mcclure@virginia.edu>
2011-06-17 18:35:23 -0400

    code structure setup for flexible logo effects

--- links/static/js/Letterer.js
+++ links/static/js/Letterer.js
@@ -3,7 +3,9 @@ var Letterer = new Class ({
     Implements: [Options, Events],
 
     options: {
-
+        fade_duration: 300,
+        fps: 100,
+        transition: 'quad:out'
     },
 
     initialize: function(div_names, id_prefix, options) {
@@ -11,9 +13,28 @@ var Letterer = new Class ({
         this.setOptions(options);
         this.id_prefix = id_prefix;
         this.divs = this.get_div_dom(div_names);
-        this.letters = this.split_letters(this.divs);
+        this.split_letters();
+        this.set_all_letter_tweens({
+            duration: this.options.fade_duration,
+            fps: this.options.fps,
+            transition: this.options.transition
+        });
 
     },
+
+    set_all_letter_tweens: function(settings) {
+
+        Array.each(this.letters, function(letter) {
+            this._set_single_letter_tween(letter, settings);
+        }.bind(this));
+
+    },
+
+    _set_single_letter_tween: function(letter, settings) {
+
+        letter.set('tween', settings);
+
+    }.protect(),
 
     get_div_dom: function(div_names) {
 
@@ -28,6 +49,7 @@ var Letterer = new Class ({
 
     split_letters: function() {
 
+        this.letters = [];
         var letter_count = 0;
 
         Array.each(this.divs, function(div) {
@@ -43,11 +65,21 @@ var Letterer = new Class ({
                     'id': this.id_prefix + '_' + i
                 }).inject(div);
 
+                this.letters.push(span);
+
             }
 
             letter_count += starting_text.length;
 
-        });
+        }.bind(this));
+
+    },
+
+    shift_letter_color: function(letter, color) {
+
+    },
+
+    pop_letter_color: function(letter, color) {
 
     },
 
@@ -62,6 +94,7 @@ var Letterer = new Class ({
 
         if (this.sequential_counter < this.letters.length) {
             this.fireEvent('sequentialStep', this.letters[this.sequential_counter]);
+            this.sequential_counter++;
             this._do_sequential_iterate_step(delay);
         }
 
@@ -74,7 +107,7 @@ var Letterer = new Class ({
     iterate_shuffled: function(delay) {
 
         this.shuffled_counter = 0;
-        this.shuffeled_letters = this._shuffle_array(this.letters);
+        this.shuffled_letters = this._shuffle_array(this.letters);
         this._do_shuffled_iterate_step(delay);
 
     },
@@ -83,7 +116,8 @@ var Letterer = new Class ({
 
         if (this.shuffled_counter < this.shuffled_letters.length) {
             this.fireEvent('shuffledStep', this.shuffled_letters[this.shuffled_counter]);
-            this._do_shuffled_iterate_step(delay);
+            this.shuffled_counter++;
+            this._do_shuffled_iterate_step.delay(delay, this, delay);
         }
 
         else {
--- links/static/js/LogoFX.js
+++ links/static/js/LogoFX.js
@@ -3,18 +3,60 @@ var LogoFX = new Class ({
     Implements: [Options, Events],
 
     options: {
-
+        dighum_div_name: 'logospan_digitalhumanities',
+        links_div_name: 'logospan_links',
+        arrow_div_name: 'logospan_arrow'
     },
 
     initialize: function(options) {
 
         this.setOptions(options);
 
+        this.divs = {
+            dighum: document.id(this.options.dighum_div_name),
+            links: document.id(this.options.links_div_name),
+            arrow: document.id(this.options.arrow_div_name)
+        };
+
+        this.original_texts = {
+            dighum: this.divs.dighum.get('text'),
+            links: this.divs.links.get('text'),
+            arrow: this.divs.arrow.get('text')
+        };
+
+    },
+
+    reset_divs: function() {
+
+        this.divs.dighum.set('html', '').set('text', this.original_texts.dighum);
+        this.divs.links.set('html', '').set('text', this.original_texts.links);
+        this.divs.arrow.set('html', '').set('text', this.original_texts.arrow);
+
     },
 
     materialize_logo: function() {
 
+        var logo_letterer = new Letterer(
+            [this.options.dighum_div_name, this.options.links_div_name],
+            'logoletters',
+            {
+                onShuffledStep: function(letter) {
+                    // console.log(letter);
+                }
+            }
+        );
 
+        var arrow_letterer = new Letterer(
+            [this.options.arrow_div_name],
+            'arrowletters',
+            {
+                onSequentialStep: function(letter) {
+
+                }
+            }
+        );
+
+        logo_letterer.iterate_shuffled(20);
 
     },
 
@@ -23,7 +65,7 @@ var LogoFX = new Class ({
 
 // dev usage
 
-// window.addEvent('domready', function() {
-//     var fx = new InterfaceFX;
-//     fx.logoAppear();
-// });
+window.addEvent('domready', function() {
+    var logofx = new LogoFX;
+    logofx.materialize_logo();
+});
