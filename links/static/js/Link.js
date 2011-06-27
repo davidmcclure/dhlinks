7b8795dd4bd7c15cd61ccc7274bffd5a6741395d
David McClure <david.mcclure@virginia.edu>
2011-06-27 09:23:03 -0400

    added disable select class

---
+++ links/static/js/Link.js
@@ -0,0 +1,21 @@
+var Link = new Class ({
+
+    Implements: [Options, Events],
+
+    options: {
+
+    },
+
+    initialize: function(options) {
+
+        this.setOptions(options);
+
+    },
+
+
+});
+
+// dev usage
+window.addEvent('domready', function() {
+
+});
