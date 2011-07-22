c8cab7882c897cdd7ab74ffd99ab56afa5dc99ea
David McClure <david.mcclure@virginia.edu>
2011-07-22 13:14:26 -0400

    working on comment markup base

---
+++ links/static/js/_comments_deployment.js
@@ -0,0 +1,7 @@
+window.addEvent('domready', function() {
+
+    this.logo = new Logo('logo', 'logospan_dighum', 'logospan_links', 'logospan_arrow');
+
+    this.navigation = new Navigation('navigation-link');
+
+});
