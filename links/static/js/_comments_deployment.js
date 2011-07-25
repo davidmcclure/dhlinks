88965b92a2c2766601524d3e89b0cf7640f2e421
David McClure <david.mcclure@virginia.edu>
2011-07-24 22:41:10 -0400

    funky changes

--- links/static/js/_comments_deployment.js
+++ links/static/js/_comments_deployment.js
@@ -1,11 +1,3 @@
 window.addEvent('domready', function() {
 
-    this.logo = new Logo('logo', 'logospan_dighum', 'logospan_links', 'logospan_arrow');
-
-    this.navigation = new Navigation('navigation-link');
-
-    this.tags = new Tags('tag');
-
-    this.links = new Links('link-container');
-
 });
