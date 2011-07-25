c690028a98a9eb974e1c581802e71d7ead2b1a90
David McClure <david.mcclure@virginia.edu>
2011-07-24 20:33:32 -0400

    comments styling tweaks, fixed user-vote data in age sort

---
+++ links/migrations/0006_auto__add_field_comment_user.py
@@ -0,0 +1,110 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Comment.user'
+        db.add_column('links_comment', 'user', self.gf('django.db.models.fields.related.ForeignKey')(default=2, to=orm['auth.User']), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Comment.user'
+        db.delete_column('links_comment', 'user_id')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'links.comment': {
+            'Meta': {'object_name': 'Comment'},
+            'comment': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'mastercomment'", 'null': 'True', 'to': "orm['links.Comment']"}),
+            'post_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'submission': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['links.Submission']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'links.commentvote': {
+            'Meta': {'object_name': 'CommentVote', '_ormbases': ['links.Vote']},
+            'comment': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['links.Comment']"}),
+            'vote_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['links.Vote']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'links.submission': {
+            'Meta': {'object_name': 'Submission'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'post_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'links.submissionvote': {
+            'Meta': {'object_name': 'SubmissionVote', '_ormbases': ['links.Vote']},
+            'submission': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['links.Submission']"}),
+            'vote_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['links.Vote']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'links.tag': {
+            'Meta': {'object_name': 'Tag'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'tag': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'links.tagsubmission': {
+            'Meta': {'object_name': 'TagSubmission'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'submission': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['links.Submission']"}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['links.Tag']"})
+        },
+        'links.userprofile': {
+            'Meta': {'object_name': 'UserProfile'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        },
+        'links.vote': {
+            'Meta': {'object_name': 'Vote'},
+            'direction': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'submit_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['links']
