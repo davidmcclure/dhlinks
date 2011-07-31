cbed938821ca7adbf1361fcfaff78b1804d0236b
David McClure <david.mcclure@virginia.edu>
2011-07-31 19:27:54 -0400

    added remember me checkbox to login form

--- links/forms.py
+++ links/forms.py
@@ -40,6 +40,7 @@ class LoginForm(forms.Form):
 
     username = forms.CharField(required=True)
     password = forms.CharField(required=True, widget=forms.PasswordInput)
+    remember_me = forms.BooleanField(required=False, label='remember me')
 
 
 class RegisterForm(forms.Form):
