69b96b7863d90ef40538f76ae3b342d7904ea13c
David McClure <david.mcclure@virginia.edu>
2011-08-07 12:08:31 -0400

    Revert "js changes to be rolled back"
    This reverts commit 1cb517db99c6d443ea154cf583795934b473e3ad.

--- links/static/js/Tags.js
+++ links/static/js/Tags.js
@@ -3,10 +3,10 @@ var Tags = new Class ({
     Implements: [Options, Events],
 
     options: {
-        'orange': '#f7ba36',
-        'blue': '#2b7bff',
-        'gray': '#9b9b9b',
-        'light_blue': '#9cc1ff'
+        orange: '#f7ba36',
+        blue: '#2b7bff',
+        gray: '#9b9b9b',
+        light_blue: '#9cc1ff'
     },
 
     initialize: function(tag_class, options) {
