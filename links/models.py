b1886b5d6fffce1cdb8d845951304e043ada5d0c
David McClure <david.mcclure@virginia.edu>
2011-08-08 15:40:45 -0400

    err magichands

--- links/models.py
+++ links/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Count
 from django.db.models import Min
 from django.db.models import Max
 from django.contrib.auth.models import User
@@ -88,18 +89,19 @@ class SubmissionManager(models.Manager):
             # Get everything.
             objects = self.model.objects.all()
 
+        if sort == 'comments':
+            objects = objects.annotate(num_comments=Count('comment')).filter(num_comments__gt=0)
+
         # Iterate over the rows; add has-the-user-voted? attribute and, if sort
         # is 'comment,' filter out submissions without comments.
 
-        for row in objects:
-            submsision = self.model.objects.get(id = row.id)
-            row.has_voted = submsision.user_has_voted(user)
-            if sort == 'comments' and row.comment_set.count() == 0:
-                pass
-            else: result_list.append(row)
+        if user.is_authenticated():
+            objects = objects.annotate(has_voted=SubmissionVote.objects.filter(user = user, submission = self))
+        # for row in objects:
+        #     row.has_voted = row.user_has_voted(user)
 
         # Apply the sorting function and slice the sorted set.
-        sorted_links = sorted(result_list, key = SubmissionManager.SORT_FUNCS[sort], reverse = True)
+        sorted_links = sorted(objects, key = SubmissionManager.SORT_FUNCS[sort], reverse = True)
         sliced_links = sorted_links[((page - 1) * SubmissionManager.LINKS_PER_PAGE):(page * SubmissionManager.LINKS_PER_PAGE)]
 
         # Figure out whether there are more links beyond those contained in the
